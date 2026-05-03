562d2a2458704d49fc6481309c0b96b4fbc9e0f3
leo <leomaxmonte14@gmail.com>
2026-05-03 01:41:34 -0300

    readme + pl

---
+++ starwars.pl
@@ -0,0 +1,135 @@
+%%% PERSONAGENS %%%
+personagem(luke_skywalker, human, tatooine).
+personagem(leia_organa, human, alderaan).
+personagem(darth_vader, human, tatooine).
+personagem(yoda, yodas_species, unknown).
+personagem(han_solo, human, corellia).
+personagem(chewbacca, wookiee, kashyyyk).
+personagem(obi_wan_kenobi, human, stewjon).
+personagem(emperor_palpatine, human, naboo).
+personagem(anakin_skywalker, human, tatooine).
+personagem(padme_amidala, human, naboo).
+personagem(qui_gon_jinn, human, coruscant).
+personagem(mace_windu, human, haruun_kal).
+personagem(jar_jar_binks, gungan, naboo).
+personagem(count_dooku, human, serenno).
+personagem(rey, human, jakku).
+personagem(finn, human, unknown).
+personagem(poe_dameron, human, yavin_4).
+personagem(kylo_ren, human, chandrila).
+personagem(bb_8, droid, unknown).
+personagem(c_3po, droid, tatooine).
+personagem(r2_d2, droid, naboo).
+personagem(lando_calrissian, human, socorro).
+personagem(boba_fett, human, kamino).
+personagem(jango_fett, human, concord_dawn).
+personagem(general_hux, human, arkanis).
+personagem(rose_tico, human, hays_minor).
+personagem(supreme_leader_snoke, unknown, unknown).
+personagem(maz_kanata, unknown, takodana).
+personagem(aayla_secura, twilek, ryloth).
+personagem(admiral_ackbar, mon_calamari, mon_cala).
+personagem(admiral_piett, human, axxila).
+personagem(bail_organa, human, alderaan).
+personagem(barriss_offee, mirialan, mirial).
+personagem(biggs_darklighter, human, tatooine).
+personagem(bib_fortuna, twilek, ryloth).
+personagem(bossk, trandoshan, trandosha).
+personagem(captain_phasma, human, parnassos).
+personagem(dengar, human, corellia).
+personagem(greedo, rodian, rodia).
+personagem(hera_syndulla, twilek, ryloth).
+personagem(jabba_the_hutt, hutt, nal_hutta).
+personagem(kanan_jarrus, human, coruscant).
+personagem(kit_fisto, nautolan, glee_anselm).
+personagem(lobot, human, bespin).
+personagem(mon_mothma, human, chandrila).
+personagem(nien_nunb, sullustan, sullust).
+personagem(plo_koon, kel_dor, dorin).
+personagem(saw_gerrera, human, onderon).
+personagem(sebulba, dug, malastare).
+personagem(shaak_ti, togruta, shili).
+personagem(shmi_skywalker, human, tatooine).
+personagem(wedge_antilles, human, corellia).
+personagem(zam_wesell, clawdite, zolan).
+personagem(ahsoka_tano, togruta, shili).
+personagem(asajj_ventress, dathomirian, dathomir).
+personagem(cad_bane, duros, duro).
+personagem(captain_rex, human, kamino).
+personagem(savage_opress, dathomirian, dathomir).
+personagem(hondo_ohnaka, weequay, sriluur).
+personagem(pong_krell, besalisk, ojom).
+personagem(mother_talzin, dathomirian, dathomir).
+personagem(ezra_bridger, human, lothal).
+personagem(sabine_wren, human, mandalore).
+personagem(kallus, human, coruscant).
+personagem(grand_admiral_thrawn, chiss, csilla).
+personagem(jyn_erso, human, vallt).
+personagem(cassian_andor, human, fest).
+personagem(orson_krennic, human, lexrul).
+personagem(bodhi_rook, human, jedha).
+personagem(baze_malbus, human, jedha).
+personagem(galen_erso, human, grange).
+personagem(admiral_raddus, mon_calamari, mon_cala).
+personagem(dryden_vos, human, corellia).
+personagem(qira, human, corellia).
+personagem(enfys_nest, human, unknown).
+personagem(l3_37, droid, unknown).
+personagem(beckett, human, glee_anselm).
+personagem(val, human, unknown).
+personagem(rio_durant, ardennian, ardennia).
+
+%%% PLANETAS %%%
+planeta(tatooine, 200000).
+planeta(alderaan, 2000000000).
+planeta(naboo, 4500000000).
+planeta(corellia, 3000000000).
+planeta(kashyyyk, 45000000).
+planeta(kamino, 1000000000).
+planeta(mon_cala, 27000000000).
+planeta(geonosis, 100000000000).
+planeta(yavin_4, 1000).
+planeta(rodia, 1300000000).
+planeta(coruscant, 1000000000000).
+
+%%% ESPECIES %%%
+%%% ESPECIES %%%
+especie(human, mammal, 79.0).
+especie(yodas_species, unknown, 900.0).
+especie(wookiee, mammal, 400.0).
+especie(gungan, amphibian, 70.0).
+especie(twilek, mammal, 80.0).
+especie(mon_calamari, amphibian, 70.0).
+especie(rodian, reptilian, 78.0).
+especie(trandoshan, reptilian, 70.0).
+especie(kel_dor, mammal, 70.0).
+especie(nautolan, amphibian, 80.0).
+especie(togruta, mammal, 94.0).
+especie(hutt, gastropod, 1000.0).
+especie(zabrak, mammal, 80.0).
+especie(muun, mammal, 100.0).
+especie(geonosian, insectoid, 90.0).
+especie(chiss, mammal, 80.0).
+especie(mirialan, mammal, 70.0).
+especie(kaminoan, amphibian, 80.0).
+especie(chadra_fan, mammal, 50.0).
+especie(dathomirian, hybrid, 60.0).
+especie(duros, reptilian, 70.0).
+especie(ewok, mammal, 50.0).
+especie(gamorrean, mammal, 45.0).
+especie(ithorian, mammal, 85.0).
+especie(jawa, mammal, 80.0).
+especie(klatooinian, reptilian, 90.0).
+especie(nikto, reptilian, 68.0).
+especie(quarren, amphibian, 79.0).
+especie(sullustan, mammal, 70.0).
+especie(talz, mammal, 60.0).
+especie(tusken_raider, mammal, 70.0).
+especie(ugnaught, mammal, 200.0).
+especie(weequay, mammal, 91.0).
+especie(zygerrian, mammal, 85.0).
+especie(jedi, mammal, 120.0).
+especie(mandalorian, mammal, 80.0).
+especie(yarkora, mammal, 200.0).
+especie(toydarian, mammal, 91.0).
+especie(kaminoan, amphibian, 80.0).
